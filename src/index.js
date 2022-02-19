279485a689c54b0749787a17d97c644e1accc5da
Bappa Dey <bappa.dey@einzigtech.com>
2022-02-20 01:43:32 +0530

    Creat new dev script in pakage.json for development mode

---
+++ src/index.js
@@ -0,0 +1,9 @@
+console.log('Hello web developer');
+alert("Hello World");
+const handleClick = () => {
+    let test = ['ramen','suman'];
+    test.map(val => {
+        console.log(val);
+    })
+}
+handleClick();
